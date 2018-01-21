f029c4bf6de657a505dda1b7a8d2b5a57556bd8b
Wiktor <wiktorbalaban@gmail.com>
2018-01-21 14:39:27 +0100

    Menu  zrobione.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Przyrost3';
+  isArenaVisible: boolean;
+  isNicknameVisible: boolean;
+
+  ngOnInit() {
+    this.isArenaVisible = false;
+    this.isNicknameVisible = false;
+  }
+
+  switchValue(isObjVisible: boolean): boolean {
+    return !isObjVisible;
+  }
 }
